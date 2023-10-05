e92befa4c1a995870e2e49b998c6bf8c31d831ce
stevieg01 <steven2512@icloud.com>
2023-10-05 12:18:51 -0400

    SnakeUpdate

---
+++ My project/Assets/Food.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    //variables 
+    public BoxCollider2D grid;   //
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RandomPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Function to randomize the position of the food
+    private void RandomPos()
+    {
+        Bounds bounds = grid.bounds;                         // declare the limits of the space 
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);     // give a random value to x within the limit 
+        float y = Random.Range(bounds.min.y, bounds.max.y);    // give a random value to y within the limit
+
+        transform.position = new Vector2(Mathf.Round(x), Mathf.Round(y));  // round values, change position of the food
+
+    }
+
+    //Function for collison
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        RandomPos();
+    }
+}
--- My project/Assets/SnakeMove.cs
+++ My project/Assets/SnakeMove.cs
@@ -1,66 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SnakeMove : MonoBehaviour
 {
-    //variable 
+    //variables
     private Vector2 direction; //control direction of movement
 
-    List<Transform> segments;              //contrp; direction of movement
-    public Transform bodyPrefab;           //variable to store the body 
+    List<Transform> segments;    // variable to store all the parts if the body of the snake
+    public Transform bodyPrefab;   //variable to store the body 
+
 
     // Start is called before the first frame update
     void Start()
     {
-        segments = new List<Transform>();  //create a new list
-        segments.Add(transform);           //add the head of the snake to the lis
+        segments = new List<Transform>();      //create a new list
+        segments.Add(transform);               // add the head of the snake to th list 
     }
 
     // Update is called once per frame
     void Update()
     {
-        // change direction of snake
-        if (Input.GetKeyDown(KeyCode.W))     //When key 'W' is pressed
+        // change direction of the snake
+        if (Input.GetKeyDown(KeyCode.W)) //when W key is pressed
         {
-            direction = Vector2.up;         //go up
+            direction = Vector2.up; // go up
         }
-        else if (Input.GetKeyDown(KeyCode.S))    //When 'S' Key is pressed
+        else if (Input.GetKeyDown(KeyCode.S)) //when S key is pressed
         {
-            direction = Vector2.down;      //go down
+            direction = Vector2.down; // go down
         }
-        else if (Input.GetKeyDown(KeyCode.A))   //When 'A' Key is pressed
+        else if (Input.GetKeyDown(KeyCode.A)) //when A key is pressed
         {
-            direction = Vector2.left;      //go left
+            direction = Vector2.left; // go left
         }
-        else if (Input.GetKeyDown(KeyCode.D))  //when 'D' is pressed
+        else if (Input.GetKeyDown(KeyCode.D)) // when D key is pressed
         {
-            direction = Vector2.right;     //go 
+            direction = Vector2.right; // go right 
         }
     }
 
     // FixedUpdate is called at a fix interval
     void FixedUpdate()
     {
-        this.transform.position = new Vector2(                     //get the position
-            Mathf.Round(this.transform.position.x) + direction.x,  //round 
-            Mathf.Round(this.transform.position.y) + direction.y
+
+        //move the body of the snake 
+        for (int i = segments.Count - 1; i > 0; i--)
+        {
+            segments[i].position = segments[i - 1].position;
+        }
+
+
+        //move the snake                                                    
+        this.transform.position = new Vector2(                    //get the position   
+            Mathf.Round(this.transform.position.x) + direction.x, //round the number add value to x
+            Mathf.Round(this.transform.position.y) + direction.y  // round the number add value to y
             );
     }
-    //Function to make the snake grow
+    //Function to make the snak egrow
     void Grow()
     {
-        Transform segment = Instantiate(this.bodyPrefab);            //get the position
-        segment.position = segments[segments.Count - 1].position;   //round the number ass value to X
-        segments.Add(segment);                                      //round the number ass value to Y
+        Transform segment = Instantiate(this.bodyPrefab);         //create a new body part
+        segment.position = segments[segments.Count - 1].position;  // position it on the back of the snake 
+        segments.Add(segment);                                         // add it to the list
     }
 
-    //Function for collisio
+    //Function for collision 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "food")
+        if (other.tag == "Food")
         {
             Grow();
+            Debug.Log("Hit");
+        }
+        else if (other.tag == "Obstacle")         //so the player loses when they hit the wall
+        {
+            Debug.Log("Hit");
+            SceneManager.LoadScene("EndScene");  //change end scene
         }
     }
 }
